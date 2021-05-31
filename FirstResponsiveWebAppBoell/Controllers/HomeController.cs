c7efa9138ae5e00109a17490a87d0df33600c462
Colby Boell <cmboell1221@gmail.com>
2021-05-31 14:01:38 -0500

    Add project files.

---
+++ FirstResponsiveWebAppBoell/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppBoell.Models;
+
+//in charge of flow of data between view and model
+namespace FirstResponsiveWebAppBoell.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet] // displays blank URL to the user
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View(); //returns view
+        }
+
+        [HttpPost] //processes data submitted by user
+        public IActionResult Index(DetermineAgeModel model)
+        {
+            if (ModelState.IsValid) //if input is valid
+            {
+                ViewBag.FV = model.AgeThisYear(); //displays result
+            }
+            else //if input is not valid
+            {
+                ViewBag.FV = 0; //displays if invalid input
+            }
+            return View(model); //returns/displays view with results and user input
+        }
+
+    }
+}
---
+++ FirstResponsiveWebAppBoell/Models/DetermineAgeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+//page that updates data and applies business rules, validates data
+namespace FirstResponsiveWebAppBoell.Models
+{
+    public class DetermineAgeModel
+    {
+        //attributes
+        [Required(ErrorMessage = "Please enter your name.")] // makes name a required field, error message if name isnt entered
+        public String Name { get; set; }
+        [Required(ErrorMessage = "Please enter your birth year.")] //makes birth year a required field, error message if birth year isnt entered
+        [Range(1800, 2021, ErrorMessage = "Enter a birth year between 1800 and 2021.")]/* gives birth year a set range, error
+                                                                                        * message if entered birth year is out of range*/
+        public int? BirthYear { get; set; }
+
+        //method for determining what age will be in 2021
+        public int? AgeThisYear()
+        {
+            //variables within the method
+            const int CURRENT_YEAR = 2021;
+            int? AgeFor2021 = CURRENT_YEAR - BirthYear;
+            return AgeFor2021; //returns value
+        }
+    }
+}
